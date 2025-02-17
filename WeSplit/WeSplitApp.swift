487bfa9903753e5c70216e01c39a63bc9a1fa877
ilkinsufi <ilkinsufi@gmail.com>
2025-02-17 09:57:50 +0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ilkin on 17.02.25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeSplitTests.swift
+//  WeSplitTests
+//
+//  Created by Ilkin on 17.02.25.
+//
+
+import Testing
+
+struct WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
